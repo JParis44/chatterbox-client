49633780ec3268532364050e07fdc449738a9f2a
JParis44 <j.paris.caldwell@gmail.com>
2015-06-22 19:26:15 -0500

    Usernames shown. User-generated messages can be sent.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,61 +1,82 @@
-var app = {};
+$(document).ready(function(){
+  var app = {};
 
-app.messages = [];
+  app.messages = [];
 
-app.init = function() {
-  // What should I be doing you guys?
+  app.init = function() {
+    // What should I be doing you guys?
 
-};
+  };
 
-app.send = function(message) {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
+  app.send = function(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  };
 
-app.fetch = function() {
-  $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    success: function (data) {
-      console.log('chatterbox: Message fetched');
-      app.messages = data.results;
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to fetch message');
-    }
-  });
-};
+  app.fetch = function() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      success: function (data) {
+        console.log('chatterbox: Message fetched');
+        app.messages = data.results;
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to fetch message');
+      }
+    });
 
-app.clearMessages = function() {
-  $('#chatterbox').empty();
-};
+    //Add filter for current room
+  };
 
-app.addMessage = function() {};
+  app.clearMessages = function() {
+    $('#chatterbox').empty();
+  };
 
-app.addRoom = function() {};
+  app.addMessage = function() {
+    var username = window.location.search.substring(window.location.search.indexOf('=')+1);
+    var message = {
+      username: username,
+      text: $('#input').val(),
+      roomname: 'main'
+    };
+    app.send(message);
+  };
 
-app.update = function () {
-  app.clearMessages();
-  $.each(app.messages, function(i, message){
-    var $message = $('<div class="message"></div>').text(message.text === undefined ? '--------' : message.text);
-    $('#chatterbox').append($message);
-  });
-  app.fetch();
-};
+  app.addRoom = function() {
+    //Set up a new room.
+  };
 
+  app.update = function () {
+    app.clearMessages();
+    $.each(app.messages, function(i, message){
+      var $message = $('<div class="message"></div>')
+      var $text = $('<div></div>').text(message.text === undefined ? '--------' : message.text);
+      var $user = $('<div></div>').text(message.username === undefined ? 'WHO ARE YOU?' : message.username);
+      $message.append($user).append($text);
+      $('#chatterbox').append($message);
 
-setInterval(app.update, 10000);
+    });
+    app.fetch();
+  };
+
+  $('.send').on('click', function(e) {
+    app.addMessage();
+  })
+
+  setInterval(app.update, 10000);
+});
